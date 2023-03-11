105f4e3826086183ef7ea59edbd43896a75a996e
mochikoAsTech <takeMe2theStarlet@gmail.com>
2023-03-11 16:00:58 +0900

    ChatGPTのコード入れる前に動いてた状態を一度コミット。

--- articles/article.re
+++ articles/article.re
@@ -661,6 +661,8 @@ LINE Creative Labを使うと、画像メッセージとして送るような素
 
 === Python
 
+ * @<href>{https://github.com/line/line-bot-sdk-python}
+
  1. mkdir python
  1. cd python
  1. pip install line-bot-sdk -t . --no-user
@@ -674,6 +676,21 @@ LINE Creative Labを使うと、画像メッセージとして送るような素
 
 ディレクトリをline-bot-sdk-pythonにしてたらこれでひっかかった
  * @<href>{https://aws.amazon.com/jp/premiumsupport/knowledge-center/lambda-import-module-error-python/}
+
+ 1. OpenAIでアカウント作る
+ 1. SECRET_KEYを取得する
+ 1. mkdir python
+ 1. cd python
+ 1. pip install openai -t . --no-user
+
+ERROR: pip's dependency resolver does not currently take into account all the packages that are installed. This behaviour is the source of the following dependency conflicts.
+line-bot-sdk 2.4.1 requires aiohttp==3.8.3, but you have aiohttp 3.8.4 which is incompatible.
+line-bot-sdk 2.4.1 requires requests==2.28.1, but you have requests 2.28.2 which is incompatible.
+Successfully installed aiohttp-3.8.4 aiosignal-1.3.1 async-timeout-4.0.2 attrs-22.2.0 certifi-2022.12.7 charset-normalizer-3.1.0 colorama-0.4.6 frozenlist-1.3.3 idna-3.4 multidict-6.0.4 openai-0.27.2 requests-2.28.2 tqdm-4.65.0 urllib3-1.26.15 yar
+
+pip check
+No broken requirements found.
+なので無視してそのまま行く
 
 
 
--- articles/linebot.py
+++ articles/linebot.py
@@ -14,9 +14,11 @@ from linebot.exceptions import (
     LineBotApiError, InvalidSignatureError
 )
 
-# WARNING, ERROR, CRITICALのログメッセージを拾うように設定する
+# ERROR以上のログメッセージを拾うように設定する
 logger = logging.getLogger()
-logger.setLevel(logging.WARNING)
+logger.setLevel(logging.ERROR)
+# 受け取ったWebhookのJSONを目視確認したいときは、INFO以上のログメッセージを拾うようにする
+# logger.setLevel(logging.INFO)
 
 # 環境変数からチャネルアクセストークンとチャネルシークレットを取得する
 CHANNEL_SECRET = os.getenv('CHANNEL_SECRET')
@@ -33,52 +35,55 @@ if CHANNEL_ACCESS_TOKEN is None:
         'LINE_CHANNEL_ACCESS_TOKEN is not defined as environmental variables.')
     sys.exit(1)
 
+line_bot_api = LineBotApi(CHANNEL_ACCESS_TOKEN)
+handler = WebhookHandler(CHANNEL_SECRET)
+
 
 def lambda_handler(event, context):
-    for message_event in json.loads(event['body'])['events']:
-        headers = {
-            'Content-Type': 'application/json',
-            'Authorization': 'Bearer ' + CHANNEL_ACCESS_TOKEN
-        }
-        body = {
-            'replyToken': message_event['replyToken'],
-            'messages':
-            [
-                {
-                    "type": "flex",
-                    "altText": "This is a Flex Message",
-                    "contents": {
-                        "type": "bubble",
-                        "hero": {
-                            "type": "image",
-                            "url": "https://pbs.twimg.com/profile_images/1041212484/100630_170522_400x400.jpg",
-                            "size": "full",
-                            "aspectRatio": "20:13",
-                            "aspectMode": "cover"
-                        },
-                        "body": {
-                            "type": "box",
-                            "layout": "vertical",
-                            "contents": [
-                                {
-                                    "type": "text",
-                                    "text": message_event['message']['text'] + 'にゃん',
-                                    "weight": "bold",
-                                    "size": "xl"
-                                }
-                            ]
-                        }
-                    }
-                }
-            ]
-        }
 
-        req = urllib.request.Request(REPLY_URL, data=json.dumps(
-            body).encode('utf-8'), method='POST', headers=headers)
-        with urllib.request.urlopen(req) as res:
-            logger.info(res.read().decode("utf-8"))
+    # リクエストヘッダーにx-line-signatureがあることを確認
+    if "x-line-signature" in event['headers']:
+        signature = event['headers']['x-line-signature']
 
-    return {
-        'statusCode': 200,
-        'body': json.dumps('Hello from Lambda!')
+    body = event["body"]
+    # 受け取ったWebhookのJSONを目視確認できるようにINFOでログに吐く
+    logger.info(body)
+
+    response = {
+        "isBase64Encoded": false,
+        "statusCode": 200,
+        "body": "{ \"message\": \"Hello from Lambda!\" }",
+        "headers": {
+            "content-type": "application/json"
+        }
     }
+    error_response = {"isBase64Encoded": False,
+                      "statusCode": 404,
+                      "headers": {},
+                      "body": "Error"}
+
+    @handler.add(MessageEvent, message=TextMessage)
+    def handle_message(event):
+        # 応答トークンを使って応答メッセージを送る
+        line_bot_api.reply_message(
+            event.reply_token, TextSendMessage(text=event.message.text))
+
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        # 署名を検証した結果、飛んできたのがLINEプラットフォームからのWebhookでなければ500を返す
+        return error_response
+    except LineBotApiError as e:
+        # 応答メッセージを送ろうとしたがLINEプラットフォームからエラーが返ってきたらエラーを吐く
+        logger.ERROR("Got exception from LINE Messaging API: %s\n" % e.message)
+        for m in e.error.details:
+            logger.ERROR("  %s: %s" % (m.property, m.message))
+        return error_response
+
+    return response
+
+
+#    return {
+#        'statusCode': 200,
+#        'body': json.dumps('Hello from Lambda!')
+#    }
