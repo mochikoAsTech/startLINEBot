e22ad8be3a724d7784c7af7bcb92d5a780fccb70
mochikoAsTech <takeMe2theStarlet@gmail.com>
2023-03-11 20:51:09 +0900

    ChatGPTに質問を投げて回答を取得する処理を入れるとどうしても動かないのでコメントアウト。

--- articles/linebot.py
+++ articles/linebot.py
@@ -3,27 +3,25 @@ import os
 import sys
 import logging
 import urllib.request
+import openai
 
-from linebot import (
-    LineBotApi, WebhookHandler
-)
-from linebot.models import (
-    MessageEvent, TextMessage, TextSendMessage,
-)
-from linebot.exceptions import (
-    LineBotApiError, InvalidSignatureError
-)
+from linebot import LineBotApi, WebhookHandler
+from linebot.models import MessageEvent, TextMessage, TextSendMessage
+from linebot.exceptions import LineBotApiError, InvalidSignatureError
 
 # ERROR以上のログメッセージを拾うように設定する
 logger = logging.getLogger()
-logger.setLevel(logging.ERROR)
+# logger.setLevel(logging.ERROR)
 # 受け取ったWebhookのJSONを目視確認したいときは、INFO以上のログメッセージを拾うようにする
-# logger.setLevel(logging.INFO)
+logger.setLevel(logging.INFO)
 
 # 環境変数からチャネルアクセストークンとチャネルシークレットを取得する
 CHANNEL_SECRET = os.getenv('CHANNEL_SECRET')
 CHANNEL_ACCESS_TOKEN = os.getenv('CHANNEL_ACCESS_TOKEN')
 REPLY_URL = 'https://api.line.me/v2/bot/message/reply'
+# OpenAI APIキーの設定
+openai.api_key = os.getenv('SECRET_KEY')
+API_ENDPOINT = 'https://api.openai.com/v1/engine/davinci-codex/completions'
 
 # チャネルアクセストークンとチャネルシークレットが環境変数に登録されていないとエラー
 if CHANNEL_SECRET is None:
@@ -38,6 +36,18 @@ if CHANNEL_ACCESS_TOKEN is None:
 line_bot_api = LineBotApi(CHANNEL_ACCESS_TOKEN)
 handler = WebhookHandler(CHANNEL_SECRET)
 
+response = {
+    'statusCode': 200,
+    'body': json.dumps('Hello from Lambda!')
+}
+
+error_response = {
+    'statusCode': 500,
+    'body': json.dumps('Only webhooks from the LINE Platform will be accepted.')
+}
+
+# LINE Messaging APIのWebhookからのリクエストを処理する
+
 
 def lambda_handler(event, context):
 
@@ -45,23 +55,18 @@ def lambda_handler(event, context):
     if "x-line-signature" in event['headers']:
         signature = event['headers']['x-line-signature']
 
-    body = event["body"]
+    body = event['body']
     # 受け取ったWebhookのJSONを目視確認できるようにINFOでログに吐く
     logger.info(body)
 
-    response = {
-        "isBase64Encoded": false,
-        "statusCode": 200,
-        "body": "{ \"message\": \"Hello from Lambda!\" }",
-        "headers": {
-            "content-type": "application/json"
-        }
-    }
-    error_response = {"isBase64Encoded": False,
-                      "statusCode": 404,
-                      "headers": {},
-                      "body": "Error"}
+    # ChatGPTに質問を投げて回答を取得する
+    # question = event.message.text
+    # response = openai.Completion.create(
+    #    engine='davinci', prompt=question, max_tokens=1024, n=1, stop=None, temperature=0.5)
+    # answer = response.choices[0].text.strip()
+    # logger.info(answer)
 
+    # オウム返しする処理
     @handler.add(MessageEvent, message=TextMessage)
     def handle_message(event):
         # 応答トークンを使って応答メッセージを送る
@@ -81,9 +86,3 @@ def lambda_handler(event, context):
         return error_response
 
     return response
-
-
-#    return {
-#        'statusCode': 200,
-#        'body': json.dumps('Hello from Lambda!')
-#    }
